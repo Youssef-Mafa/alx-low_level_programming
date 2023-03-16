e86ade3d148108804e2ec6fb2a5117b34eec30cb
Youssef-Mafa <youssefmafanze@gmail.com>
2023-03-16 15:29:27 -0700

    test5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 /**
- * main - Entry point
+ * main - A program
  *
  * Return: Always 0 (Success)
  */
@@ -10,6 +9,5 @@ int main(void)
 {
 	char tring[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(tring);
 	return (0);
 }
