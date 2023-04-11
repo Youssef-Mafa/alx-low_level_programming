fd309408a86a5989990e960d67945aea2e6a0e90
Youssef-Mafa <youssefmafanze@gmail.com>
2023-04-10 17:40:02 -0700

    lo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this fun prints its name, fllowed by a new line
+ * @argc: count line command argument
+ * @argv: contain arg in line command
+ * return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+		return (0);
+}
