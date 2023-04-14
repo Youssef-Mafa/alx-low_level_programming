7060414ca8c52c3b36bdd8c3bb82d671fcd117d6
Youssef-Mafa <youssefmafanze@gmail.com>
2023-04-13 17:53:42 -0700

    salam

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,30 +8,30 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;
-	char *str;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (n = 0; av[i][n]; n++)
-			l++;
-	}
-	l += ac;
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (n = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-		r++;
-	}
-	if (str[r] == '\0')
-	{
-		str[r++] = '\n';
-	}
-	}
-	return (str);
+        int x, y, i = 0, j = 0;
+        char *str;
+        if (ac == 0 || av == NULL)
+                return (NULL);
+        for (x = 0; x < ac; x++)
+        {
+                for (y = 0; av[x][y]; y++)
+                        j++;
+        }
+        j += ac;
+        str = malloc(sizeof(char) * j + 1);
+        if (str == NULL)
+                return (NULL);
+        for (x = 0; x < ac; x++)
+        {
+        for (y = 0; av[x][y]; y++)
+        {
+                str[i] = av[x][y];
+                i++;
+        }
+        if (str[i] == '\0')
+        {
+                str[i++] = '\n';
+        }
+        }
+        return (str);
 }
