4e4d271554a61037bbdf911938d00fb9f5af6737
Youssef-Mafa <youssefmafanze@gmail.com>
2023-04-17 18:03:00 -0700

    salam

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,1 @@
-#include "dog.h"
-#include <stdlib.h>
 
-/**
- * free_dog - let dogs run
- * @d:the chosen dog
- */
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-		return;
-	free((*d).name);
-	free((*d).owner);
-	free(d);
-}
