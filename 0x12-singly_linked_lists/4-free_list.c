b58d602785a096b5265ebae11fdebdf2c771c579
Youssef-Mafa <youssefmafanze@gmail.com>
2023-04-27 19:56:40 -0700

    hh

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,24 +2,20 @@
 #include "lists.h"
 
 /**
- * This function frees a linked list.
- * It takes a pointer to the head of the list as input.
+
+free_list - frees a linked list
+
+@head: pointer to the head of the list
 */
 void free_list(list_t *head)
 {
-// Create a temporary pointer to traverse the list
 list_t *temp;
 
-// Loop through the list, freeing each node
 while (head != NULL)
 {
-// Save the next node in the list
 temp = head->next;
- // Free the string and the node itself
- free(head->str);
- free(head);
-
- // Move to the next node
- head = temp;
+free(head->str);
+free(head);
+head = temp;
 }
 }
