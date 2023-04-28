506a2f1363d89efea0e34fcb9ab8a1556cdb7d66
Youssef-Mafa <youssefmafanze@gmail.com>
2023-04-27 19:35:40 -0700

    hh

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,21 +11,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-unsigned int len = 0;
+    if (!head || !str)
+        return NULL;
 
-while (str[len])
-len++;
+    list_t *new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return NULL;
 
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
+    new_node->str = strdup(str);
+    if (!new_node->str) {
+        free(new_node);
+        return NULL;
+    }
 
-new->str = strdup(str);
-new->len = len;
-new->next = (*head);
-(*head) = new;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
-return (*head);
+    return new_node;
 }
-
