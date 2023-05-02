fa4bb06a25ec84330ab83d486a474c3501f80dae
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-02 10:09:46 -0700

    salam labas?

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	for (; h != NULL; h = h->next)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 
-	return count;
+	return (count);
 }
