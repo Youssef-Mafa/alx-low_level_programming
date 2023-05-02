72c2ff5e453efe5437763484617453ba8c464565
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-02 11:55:29 -0700

    alo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list
- * at the specified position
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
  * @head: pointer to the first node in the list
- * @idx: index where the new node should be added
+ * @idx: index where the new node is added
  * @n: data to insert in the new node
  *
- * Return: pointer to the new node, or NULL if failed
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/* create new node */
-	listint_t *new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
+    listint_t *new_node, *temp = *head;
 
-	if (idx == 0) {
-		/* insert at the beginning of the list */
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
+    new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+        return (NULL);
 
-	/* traverse the list to find the position to insert the new node */
-	listint_t *current = *head;
-	unsigned int i;
-	for (i = 0; current && i < idx - 1; i++)
-		current = current->next;
+    new_node->n = n;
 
-	if (i != idx - 1) {
-		/* index is out of range */
-		free(new_node);
-		return (NULL);
-	}
+    if (idx == 0)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
 
-	/* insert new node at the specified position */
-	new_node->next = current->next;
-	current->next = new_node;
+    for (unsigned int i = 0; temp; i++, temp = temp->next)
+    {
+        if (i == idx - 1)
+        {
+            new_node->next = temp->next;
+            temp->next = new_node;
+            return (new_node);
+        }
+    }
 
-	return (new_node);
+    free(new_node);
+    return (NULL);
 }
