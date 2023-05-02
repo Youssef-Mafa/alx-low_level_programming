6cb505379d64eeea95ec7d2ab9c5984bd0a60b92
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-02 10:37:21 -0700

    alo

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		listint_t *temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 
 	*head = NULL;
