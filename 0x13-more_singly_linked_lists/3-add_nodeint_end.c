fe71cb27b22470fa07f338f2992b16178e37efce
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-02 10:24:50 -0700

    salam labas?

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,26 +9,23 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
-	listint_t *temp = *head;
+	if (!head)
+		return (NULL);
 
-	new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
+	if (!*head)
+		return (*head = new);
 
+	listint_t *temp = *head;
 	while (temp->next)
 		temp = temp->next;
 
 	temp->next = new;
-
 	return (new);
 }
