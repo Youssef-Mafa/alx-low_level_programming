225723ae09ee43267ac99207f989a2704aa12bdf
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-04 16:55:02 -0700

    salama ya rbbi

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
-
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
+	if (b == NULL)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		return 0;
 	}
 
-	return (dec_val);
-}
+	unsigned int dec_val = 0;
 
+	for (int i = 0; b[i] != '\0'; i++) 
+	{
+		if (b[i] == '0' || b[i] == '1') 
+		{
+			dec_val = dec_val * 2 + (b[i] - '0');
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	return dec_val;
+}
