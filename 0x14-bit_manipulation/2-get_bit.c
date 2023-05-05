11119d9a125a2607bac7f5830747b74da711b5c7
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-04 17:26:31 -0700

    lol

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,9 +5,12 @@
  * @n: number to search
  * @index: index of the bit
  *
- * Return: value of the bit, or -1 if index is out of range
+ * Return: value of the bit, or -1 if the index is invalid
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return (index > 63) ? -1 : (n >> index) & 1;
+    if (index >= sizeof(unsigned long int) * 8)
+        return -1;
+
+    return (n >> index) & 1;
 }
