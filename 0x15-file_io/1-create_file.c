39790fbe4defb710d912a086cc373bd859cc62e4
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-08 15:58:13 -0700

    hh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,30 +2,31 @@
 
 /**
  * create_file - function that creates a file.
- *  @filename: pointer to name of file
- *  @text_content: pointer to text
- *  Return: -1 or 1
+ * @filename: pointer to name of file
+ * @text_content: pointer to text
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0;
+	int fd, bytes_written, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		while (text_content[len])
 			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(op, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_written = write(fd, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (fd == -1 || bytes_written == -1)
 		return (-1);
 
-	close(op);
+	close(fd);
 
 	return (1);
 }
