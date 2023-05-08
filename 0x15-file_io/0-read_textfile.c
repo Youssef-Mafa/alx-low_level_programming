0b997216333ad8186c695051a3a9140e2b107f56
Youssef-Mafa <youssefmafanze@gmail.com>
2023-05-08 15:33:24 -0700

    hh

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
 /**
  * read_textfile - function reads text file and prints it
  * to the POSIX standard output.
- * @filename:pointer to name of file
- * @letters:number of letters te func sould read and pirnt
- * Return: 0 or num of byte func sould read and print
+ * @filename: pointer to name of file
+ * @letters: number of letters the function should read and print
+ * Return: 0 or number of bytes the function should read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -15,19 +15,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	tab = malloc(sizeof(tab) * letters);
+	tab = malloc(sizeof(*tab) * (letters + 1));
 	if (tab == NULL)
 		return (0);
 
 	op = open(filename, O_RDONLY);
-	rd = read(op, tab, letters);
-	wr = write(STDOUT_FILENO, tab, rd);
-
-	if (op == -1 || rd == -1 || wr != rd)
-	{
+	if (op == -1) {
 		free(tab);
 		return (0);
 	}
+	rd = read(op, tab, letters);
+	if (rd == -1) {
+		free(tab);
+		close(op);
+		return (0);
+	}
+
+	tab[rd] = '\0';
+	wr = write(STDOUT_FILENO, tab, rd);
+	if (wr == -1) {
+		free(tab);
+		close(op);
+		return (0);
+	}
+
 	free(tab);
 	close(op);
 	return (wr);
