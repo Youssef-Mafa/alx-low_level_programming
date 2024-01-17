b3afc2a8aa00dfaf28fc171a91bd959d4977664a
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-01-17 12:19:24 +0100

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -15,7 +15,6 @@ int interpolation_search(int *array, size_t size, int value)
 		return (-1);
 	low = 0;
 	high = size - 1;
-
 	while ((array[high] != array[low]) &&
 		   (value >= array[low]) &&
 		   (value <= array[high]))
