b715bdc157acf6a30e7a67104207dce1677597cd
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-01-17 12:22:35 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,11 +4,9 @@
 #include <stdio.h>
 /**
  * struct listint_s - singly linked list
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- *
  * Description: singly linked list node structure
  */
 typedef struct listint_s
