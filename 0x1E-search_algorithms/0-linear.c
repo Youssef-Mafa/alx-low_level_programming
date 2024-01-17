8e433e4dd6b0a3747e15de315eaf66309de8f8a6
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-01-17 12:17:27 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,7 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches for a value in an array of integers using the
- *					Linear search algorithm
+ *Linear search algorithm
  * @array: array of ints
  * @size: size of array
  * @value: int value we are looking for
