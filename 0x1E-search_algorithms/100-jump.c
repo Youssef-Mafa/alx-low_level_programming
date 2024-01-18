a1963b314557a47f7c6f4484d0123558d85ffeb1
Youssef-Mafa <youssefmafanze@gmail.com>
2024-01-17 16:17:57 -0800

    a

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,7 +3,7 @@
 
 /**
  * jump_search - searches for a value in a sorted array of integers
- *	using the Jump search algorithm
+ *				using the Jump search algorithm
  * @array: array of ints
  * @size: size of array
  * @value: int value we are looking for
@@ -12,6 +12,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t l, r, j, i;
+
 	if (array == NULL)
 		return (-1);
 	l = 0;
