c1bd0d5b1c07969811a2728b9f5da95d33991b54
Youssef-Mafa <youssefmafanze@gmail.com>
2024-01-17 16:12:36 -0800

    a

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,61 @@
-#include "search_algos.h"
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* STRUCTS */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 
 /**
- * binary_search - earches for a value in a sorted array of integers
- * using the Binary search algorithm
- * @array: array of ints
- * @size: size of array
- * @value: int value we are looking for
- * Return: Always EXIT_SUCCESS
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
-int binary_search(int *array, size_t size, int value)
+typedef struct skiplist_s
 {
-	size_t l, r, m, i;
-	l = 0;
-	r = size - 1;
-	if (array == NULL)
-		return (-1);
-	while (l <= r)
-	{
-		printf("Searching in array:");
-		for (i = l; i <= r; i++)
-		{
-			if (i == l)
-				printf(" %d", array[i]);
-			else
-				printf(", %d", array[i]);
-		}
-		printf("\n");
-		m = (l + r) / 2;
-		if (array[m] < value)
-			l = m + 1;
-		else if (array[m] > value)
-			r = m - 1;
-		else
-			return (m);
-	}
-	return (-1);
-}
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* UTILITIES */
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
+
+#endif
