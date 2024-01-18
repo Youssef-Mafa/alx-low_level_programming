573e8d4a55890ba26de13d65ed8014d3f9a8ef7e
Youssef-Mafa <youssefmafanze@gmail.com>
2024-01-17 16:22:55 -0800

    a

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,42 +1,40 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in a sorted array of integers
- *				using the Jump search algorithm
+ * interpolation_search - searches for a value in a sorted array of integers
+ *							using the Interpolation search algorithm
  * @array: array of ints
  * @size: size of array
  * @value: int value we are looking for
  * Return: Always EXIT_SUCCESS
  */
-int jump_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l, r, j, i;
+	size_t low, high, pos;
 
 	if (array == NULL)
 		return (-1);
-	l = 0;
-	j = sqrt(size);
-	r = j;
-	while (1)
+	low = 0;
+	high = size - 1;
+
+	while ((array[high] != array[low]) &&
+		   (value >= array[low]) &&
+		   (value <= array[high]))
 	{
-		printf("Value checked array[%ld] = [%d]\n", l, array[l]);
-		if (r >= size || array[r] >= value)
-			break;
-		else if (array[r] < value)
-		{
-			l = r;
-			r += j;
-		}
+		pos = low + (((double)(high - low) /
+					  (array[high] - array[low])) *
+					 (value - array[low]));
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
+		else
+			return (pos);
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n", l, r);
-	for (i = l; i < size && i <= r; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
-			return (i);
-		}
-	}
+	pos = low + (((double)(high - low) /
+					  (array[high] - array[low])) *
+					 (value - array[low]));
+	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
 }
